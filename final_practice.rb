168312f36abe8226ca8b67954c009d26cdb31e2a
Sooyung Kim <kimx1279@gmail.com>
2023-08-04 14:04:06 -0600

    Add response to question 5

--- final_practice.rb
+++ final_practice.rb
@@ -85,14 +85,25 @@ def square(number)
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-check_stock(4, "Coffee");
+
+def check_stock(num, item)
+    if num >=4
+       return "#{item} is stocked"
+    elsif num == 0
+       return "#{item} - OUT of stock!"
+    else 
+       return "#{item} - running LOW"
+    end
+   end
+   
+puts check_stock(4, "Coffee");
 # => "Coffee is stocked"
 
-check_stock(3, "Tortillas");
+puts check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
 
-check_stock(0, "Cheese");
+puts check_stock(0, "Cheese");
 # => "Cheese - OUT of stock!"
 
-check_stock(1, "Salsa");
+puts check_stock(1, "Salsa");
 # => "Salsa - running LOW"
